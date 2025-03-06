96fa874c367b562264d6d9f472d87e9cf2d96f31
Loós András Péter <loosandraspeter@gmail.com>
2025-03-06 22:25:30 +0100

    Connecting the swagger.yaml to project

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import * as path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
 import { SecretController } from './controllers/secretController';
 
 export class App {
@@ -10,6 +13,7 @@ export class App {
     this.secretController = new SecretController();
     this.configureMiddleware();
     this.configureRoutes();
+    this.configureSwagger();
   }
 
   private configureMiddleware() {
@@ -20,5 +24,9 @@ export class App {
   private configureRoutes() {
     this.express.post('/v1/secret', this.secretController.createSecret.bind(this.secretController));
     this.express.get('/v1/secret/:hash', this.secretController.getSecret.bind(this.secretController));
+  }
+  private configureSwagger() {
+    const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+    this.express.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   }
 }
